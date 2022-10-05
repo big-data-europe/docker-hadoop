90d2f5d9e742b21edce4e1f3f01888da0b015207
zk <liuzq7@gmail.com>
2022-10-06 01:28:23 +0800

    add upload demo adn hosts config

--- java-src/hadoopdemo/src/test/java/com/zk/AppTest.java
+++ java-src/hadoopdemo/src/test/java/com/zk/AppTest.java
@@ -2,12 +2,14 @@ package com.zk;
 
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Unit test for simple App.
@@ -31,5 +33,21 @@ public class AppTest {
             fs.delete(path, true);
         }
         fs.mkdirs(path);
+    }
+
+
+    @Test
+    void upload() throws IOException {
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(new File(
+                "./data/words.txt")));
+           Path path = new Path("/zk/words2.txt");
+           FSDataOutputStream fsDataOutputStream = fs.create(path);
+           IOUtils.copyBytes(bufferedInputStream,fsDataOutputStream,configuration, true);
+    }
+
+    @Test
+    void read() {
+        Path path = new Path("/zk/words.txt");
+
     }
 }
